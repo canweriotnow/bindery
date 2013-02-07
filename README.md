1e7776fd5d3bd20efc54a8b905fd590dccbac99f
Jason Lewis <jason@decomplecting.org>
2013-02-06 21:35:26 -0500

    Add dependencies hiccup, docjure

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # bindery
 
-A Clojure library designed to ... well, that part is up to you.
+A Clojure library designed to format reports from structured data. 
 
 ## Usage
 
--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [hiccup "1.0.1"]
+                 [dk.ative/docjure "1.6.0"]])
