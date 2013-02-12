086e9e142dadd84cd435b5d24cb58476a19ce19c
Jason Lewis <jason@decomplecting.org>
2013-02-11 20:28:08 -0500

    Build csv from resultset-seq

--- src/bindery/format/csv.clj
+++ src/bindery/format/csv.clj
@@ -1,1 +1,16 @@
-(ns bindery.format.csv)
+(ns bindery.format.csv
+  (:use bindery.common)
+  (:require [clojure.string :as string]))
+
+(defn column-headers [resultset]
+  (string/join ","
+    (map #(desym %) (keys (first resultset)))))
+
+(defn csv-data [resultset]
+  (str (string/join (get-values-seq resultset)) "\n")
+
+(defn build-csv [resultset]
+  (let [headers (column-headers resultset)
+        data (csv-data resultset)]
+    (str headers "\n" data)))
+
